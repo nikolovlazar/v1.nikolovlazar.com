543f836e3728ffe8c9f8252ac4f5cd660095821a
Lazar Nikolov <lazar@codechem.com>
2021-11-15 13:08:01 +0100

    added twitch embed in 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,20 +1,30 @@
-import { VStack, Heading, Text, Link } from '@chakra-ui/react';
+import { VStack, Heading, Text, Link, AspectRatio } from '@chakra-ui/react';
 
 const NotFound = () => {
   return (
-    <VStack>
-      <Heading>Coming soon...</Heading>
-      <Text>
-        If you want to keep up with the development, make sure to follow me on{' '}
-        <Link
-          href='https://twitch.tv/nikolovlazar'
-          color='purple.500'
-          target='_blank'
-        >
-          Twitch
-        </Link>
-        .
-      </Text>
+    <VStack spacing={12} w='full'>
+      <VStack>
+        <Heading>Coming soon...</Heading>
+        <Text>
+          I&apos;m live streaming the development, so make sure to follow me on{' '}
+          <Link
+            href='https://twitch.tv/nikolovlazar'
+            color='purple.500'
+            target='_blank'
+          >
+            Twitch
+          </Link>
+          .
+        </Text>
+      </VStack>
+      <AspectRatio ratio={16 / 9} w='full'>
+        <iframe
+          src='https://player.twitch.tv/?video=v1168804058&parent=nikolovlazar.com&autoplay=false'
+          height='100%'
+          width='100%'
+          allowfullscreen='true'
+        ></iframe>
+      </AspectRatio>
     </VStack>
   );
 };
