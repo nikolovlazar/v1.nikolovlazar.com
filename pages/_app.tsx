23a01d8549ceb396b75137758a0be5543993ab4e
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2021-11-21 18:28:17 +0100

    feature: added more SEO tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,8 +11,28 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
       <NextSeo
-        title='Lazar Nikolov - Developer, Designer, DevRel, Content Creator.'
-        description='Full-stack Engineer, UI Designer, Course Creator.'
+        title='Lazar Nikolov - Developer, designer, course creator.'
+        description='Full-stack Engineer, UI Designer, and Open Source Advocate.'
+        twitter={{
+          cardType: 'summary_large_image',
+          handle: '@NikolovLazar',
+        }}
+        openGraph={{
+          url: 'https://nikolovlazar.com',
+          title: 'Lazar Nikolov - Developer, designer, course creator.',
+          description:
+            'Full-stack Engineer, UI Designer, and Open Source Advocate.',
+          locale: 'en_US',
+          images: [
+            {
+              url: 'https://nikolovlazar.com/assets/images/social.png',
+              width: 1200,
+              height: 630,
+              alt: 'Lazar Nikolov',
+              type: 'image/png',
+            },
+          ],
+        }}
       />
       <Layout>
         <Component {...pageProps} />
