df5e449dc2a39d6abdd555c0836c6dabd9609d37
Lazar Nikolov <lazar@codechem.com>
2021-11-25 10:05:54 +0100

    feature: updated mdx heading letter spacing

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -34,15 +34,19 @@ const theme = extendTheme({
   mdx: {
     h1: {
       fontSize: '3xl',
+      letterSpacing: '1px',
     },
     h2: {
       fontSize: 'xl',
+      letterSpacing: '0.8px',
     },
     h3: {
       fontSize: 'md',
+      letterSpacing: '0.6px',
     },
     h4: {
       fontSize: 'xs',
+      letterSpacing: '0.5px',
     },
   },
 });
