6b18295d9dfc7894984a4cab45bd4d32e4418a69
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2021-11-28 09:29:16 +0100

    updated polywork URL

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,6 @@ export const TWITTER_PROFILE = 'https://twitter.com/NikolovLazar';
 export const GITHUB_PROFILE = 'https://github.com/lazarnikolov94';
 export const YOUTUBE_CHANNEL =
   'https://www.youtube.com/channel/UCTexaJMnN_Pv6TVueQ61-oQ';
-export const POLYWORK_PROFILE = 'https://www.polywork.com/lazar';
+export const POLYWORK_PROFILE = 'https://polywork.com/nikolovlazar';
 export const TWITCH_CHANNEL = 'https://twitch.tv/nikolovlazar';
 export const NEWSLETTER_URL = 'http://newsletter.nikolovlazar.com';
