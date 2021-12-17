6abd999ec1af76170f6a09cfa8e205c134dab9b4
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2021-12-17 08:51:48 +0100

    isolated status indicator into its own component

--- src/components/hero-image/index.tsx
+++ src/components/hero-image/index.tsx
@@ -1,6 +1,6 @@
 import { Box, AspectRatio, Text, HStack, Flex } from '@chakra-ui/react';
 
-import status from '@/data/status';
+import StatusIndicator from '../status-indicator';
 
 const HeroImage = () => {
   return (
@@ -26,60 +26,7 @@ const HeroImage = () => {
           </video>
         </Box>
       </AspectRatio>
-      <HStack
-        sx={{
-          '@media(hover: none)': {
-            width: 'full',
-            maxWidth: 40,
-          },
-        }}
-        position='absolute'
-        bottom={0}
-        overflow='hidden'
-        borderWidth={1}
-        borderColor='gray.300'
-        borderStyle='solid'
-        height={10}
-        width={10}
-        maxWidth={10}
-        alignItems='center'
-        justifyContent='center'
-        bg='white'
-        rounded='3xl'
-        px={2}
-        role='group'
-        transitionProperty='all'
-        transitionDuration='slow'
-        transitionTimingFunction='ease-out'
-        _hover={{
-          width: 'full',
-          maxWidth: 40,
-        }}
-      >
-        <Text>{status.emoji}</Text>
-        <Text
-          isTruncated
-          overflow='none'
-          display='none'
-          opacity={0}
-          height={6}
-          transitionProperty='opacity'
-          transitionDuration='slow'
-          transitionTimingFunction='ease-out'
-          _groupHover={{
-            opacity: 1,
-            display: 'inline',
-          }}
-          sx={{
-            '@media(hover: none)': {
-              opacity: 1,
-              display: 'inline',
-            },
-          }}
-        >
-          {status.text}
-        </Text>
-      </HStack>
+      <StatusIndicator />
     </Flex>
   );
 };
