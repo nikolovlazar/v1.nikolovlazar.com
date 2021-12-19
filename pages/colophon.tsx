bce7a71adef827dcc61a0995b377c65b3fb93a9e
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2021-12-19 13:56:39 +0100

    updated colophon page

--- pages/colophon.tsx
+++ pages/colophon.tsx
@@ -27,13 +27,17 @@ const ColophonPage = () => {
           It is designed using <Link href="https://figma.com/" isExternal>Figma</Link>.
         </Text>
         <Text>
-          The source code is open source and can be found on <Link href="https://github.com/lazarnikolov94/personal-website" isExternal>GitHub</Link>.{' '}
-          The UI Design can be found on <Link href="https://www.figma.com/community/file/1014838147356550908/Personal-Website" isExternal>Figma</Link>.
-        </Text>
-        <Text>
           This website uses <Link href="https://plausible.io/nikolovlazar.com" isExternal>Plausible Analytics</Link>,{' '}
           which is compatible with the different privacy regulations such as GDPR, CCPA and PECR out of the box.{' '}
           <Link href="https://plausible.io/blog/google-analytics-cookies" isExternal>It does not use cookies</Link>, and it does not collect any personal data from the visitors either.
+        </Text>
+        <Text>
+          Everything is Open Source 💜
+        </Text>
+        <Text>
+          The source code can be found on <Link href="https://github.com/lazarnikolov94/personal-website" isExternal>GitHub</Link>.{' '}
+          The UI Design can be found on <Link href="https://www.figma.com/community/file/1014838147356550908/Personal-Website" isExternal>Figma</Link>.
+          Even the Analytics are open source: <Link href="https://plausible.io/nikolovlazar.com" isExternal>Analytics Dashboard</Link>
         </Text>
       </VStack>
     </>
