dded3e79fc55025d202c1c2af28fa43636741271
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2022-02-13 21:52:10 +0100

    fixed 404 videos randomization

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,25 @@
-import { VStack, Heading, Text, Box } from '@chakra-ui/react';
+import { VStack, Heading, Text, AspectRatio } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
 const NotFound = () => {
-  const videoVersion = Math.floor(Math.random() * 3 + 1);
+  const [videoVersion, setVideoVersion] = useState<number>();
+
+  useEffect(() => {
+    setVideoVersion((new Date().getSeconds() % 3) + 1);
+  }, [setVideoVersion]);
+
   return (
     <VStack w='full' spacing={12}>
-      <VStack spacing={4}>
-        <Box overflow='hidden' rounded='lg'>
+      <VStack w='full' spacing={4}>
+        <AspectRatio
+          as='figure'
+          flexShrink={0}
+          overflow='hidden'
+          w='full'
+          shadow='xl'
+          ratio={16 / 9}
+          rounded='lg'
+        >
           <video autoPlay loop muted playsInline>
             <source
               src={`/assets/videos/404-${videoVersion}.webm`}
@@ -16,7 +30,7 @@ const NotFound = () => {
               type='video/mp4'
             />
           </video>
-        </Box>
+        </AspectRatio>
         <Heading>Say what? 🤨</Heading>
         <Text>I&apos;m afraid this page doesn&apos;t exist.</Text>
       </VStack>
