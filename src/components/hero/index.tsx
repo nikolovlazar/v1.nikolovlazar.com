51aa630e0422b788d87488afc8c3a5b8f68a4fea
Lazar Nikolov <hello@nikolovlazar.com>
2022-05-16 09:23:02 +0200

    changed polywork link + added beerjs talk

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -6,36 +6,36 @@ import {
   Button,
   Icon,
   Link,
-} from "@chakra-ui/react";
-import { FiArrowUpRight } from "react-icons/fi";
+} from '@chakra-ui/react';
+import { FiArrowUpRight } from 'react-icons/fi';
 
 import {
   GITHUB_PROFILE,
   POLYWORK_PROFILE,
   TWITCH_CHANNEL,
   TWITTER_PROFILE,
-} from "src/constants";
-import { Link as LinkType } from "@/types/link";
-import ExternalLink from "../external-link";
-import HeroImage from "../hero-image";
-import HeroPlay from "../hero-play";
+} from 'src/constants';
+import { Link as LinkType } from '@/types/link';
+import ExternalLink from '../external-link';
+import HeroImage from '../hero-image';
+import HeroPlay from '../hero-play';
 
 type SocialLink = LinkType & { color?: string };
 
 const socialLinks: SocialLink[] = [
   {
     href: TWITTER_PROFILE,
-    label: "Twitter",
-    color: "twitter",
+    label: 'Twitter',
+    color: 'twitter',
   },
   {
     href: GITHUB_PROFILE,
-    label: "GitHub",
+    label: 'GitHub',
   },
   {
-    href: TWITCH_CHANNEL,
-    label: "Twitch",
-    color: "purple.500",
+    href: POLYWORK_PROFILE,
+    label: 'Polywork',
+    color: 'purple.500',
   },
 ];
 
@@ -44,15 +44,15 @@ const Hero = () => {
     <Stack
       as="section"
       alignItems="center"
-      direction={{ base: "column-reverse", md: "row" }}
+      direction={{ base: 'column-reverse', md: 'row' }}
       w="full"
       spacing={12}
     >
       <VStack alignItems="flex-start" w="full" spacing={3}>
         <Stack
           alignItems="center"
-          justifyContent={{ base: "center", md: "flex-start" }}
-          direction={{ base: "column", md: "row" }}
+          justifyContent={{ base: 'center', md: 'flex-start' }}
+          direction={{ base: 'column', md: 'row' }}
           w="full"
           spacing={3}
         >
@@ -63,20 +63,20 @@ const Hero = () => {
         </Stack>
         <Text as="h2" lineHeight="175%">
           I’m a full-stack engineer, a designer, and a content creator. I work
-          at <ExternalLink href="https://codechem.com">CodeChem</ExternalLink>{" "}
-          as a <strong>developer/designer</strong>, and I’m a{" "}
-          <strong>core member</strong> at{" "}
+          at <ExternalLink href="https://codechem.com">CodeChem</ExternalLink>{' '}
+          as a <strong>developer/designer</strong>, and I’m a{' '}
+          <strong>core member</strong> at{' '}
           <ExternalLink href="https://chakra-ui.com">Chakra UI</ExternalLink>,
-          contributing as a <strong>Developer Advocate</strong>. I’m also an{" "}
-          <ExternalLink href="https://egghead.io">egghead.io</ExternalLink>{" "}
+          contributing as a <strong>Developer Advocate</strong>. I’m also an{' '}
+          <ExternalLink href="https://egghead.io">egghead.io</ExternalLink>{' '}
           <strong>instructor</strong>.
         </Text>
-        <Stack direction={{ base: "column", md: "row" }} spacing={3}>
+        <Stack direction={{ base: 'column', md: 'row' }} spacing={3}>
           {socialLinks.map(({ href, label, color }) => (
             <Button
               key={href}
               as={Link}
-              justifyContent={{ base: "flex-start", md: "center" }}
+              justifyContent={{ base: 'flex-start', md: 'center' }}
               px={4}
               color={color}
               href={href}
