db9c6411d8cafc30b4df096a838ef39353fd348a
Lazar Nikolov <hello@nikolovlazar.com>
2022-10-25 23:17:01 +0400

    added vercel analytics

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,6 +3,7 @@ import { VStack, Container } from '@chakra-ui/react';
 
 import Header from './header';
 import Footer from './footer';
+import { AnalyticsWrapper } from '../analytics/analytics-wrapper';
 
 type Props = PropsWithChildren<{}>;
 
@@ -11,18 +12,19 @@ const Layout = ({ children }: Props) => {
     <>
       <Header />
       <Container
-        display="flex"
-        maxW="container.md"
+        display='flex'
+        maxW='container.md'
         minH={{ base: 'auto', md: '100vh' }}
         px={{ base: 4, lg: 0 }}
         centerContent
       >
-        <VStack alignItems="stretch" flex={1} w="full" spacing={16}>
-          <VStack as="main" flex={1} w="full" spacing={16}>
+        <VStack alignItems='stretch' flex={1} w='full' spacing={16}>
+          <VStack as='main' flex={1} w='full' spacing={16}>
             {children}
           </VStack>
           <Footer />
         </VStack>
+        <AnalyticsWrapper />
       </Container>
     </>
   );
