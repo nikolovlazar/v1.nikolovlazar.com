6c4878fbdcd836bc80cf39c58c6acf1a281f0101
Lazar Nikolov <hello@nikolovlazar.com>
2022-10-28 23:38:43 +0400

    new profile pic who dis

--- src/components/hero-image/index.tsx
+++ src/components/hero-image/index.tsx
@@ -1,19 +1,20 @@
-import { Box, AspectRatio, Flex } from "@chakra-ui/react";
+import { Box, AspectRatio, Flex } from '@chakra-ui/react';
 
-import StatusIndicator from "../status-indicator";
+import Image from '@/components/image';
+import StatusIndicator from '../status-indicator';
 
 const HeroImage = () => {
   return (
-    <Flex position="relative" justify="center" pb={4}>
-      <AspectRatio as="figure" flexShrink={0} w={56} h={56} ratio={1}>
-        <Box overflow="hidden" rounded="full">
-          <video autoPlay loop muted playsInline>
-            <source
-              src="/assets/videos/lazar-croissant.webm"
-              type="video/webm"
-            />
-            <source src="/assets/videos/lazar-croissant.mp4" type="video/mp4" />
-          </video>
+    <Flex position='relative' justify='center' pb={4}>
+      <AspectRatio as='figure' flexShrink={0} w={56} h={56} ratio={1}>
+        <Box overflow='hidden' rounded='full'>
+          <Image
+            alt='A photo of Lazar Nikolov'
+            src='/assets/images/lazar.jpg'
+            width={224}
+            height={224}
+            objectFit='contain'
+          />
         </Box>
       </AspectRatio>
       <StatusIndicator />
