d6cde6c4d06b63650b6b4395d1bb932c189f72fb
Lazar Nikolov <hello@nikolovlazar.com>
2023-04-24 01:11:58 -0400

    adding a not maintained banner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,43 +1,66 @@
-import { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
-import { NextSeo } from "next-seo";
-import "cal-sans";
+import { AppProps } from 'next/app';
+import { Center, ChakraProvider, chakra } from '@chakra-ui/react';
+import { NextSeo } from 'next-seo';
+import 'cal-sans';
 
-import theme from "../src/theme";
-import Layout from "@/components/layout";
-import CmdPalette from "@/components/cmd-palette";
-import CmdPaletteProvider from "src/providers/cmd-palette-provider";
+import theme from '../src/theme';
+import Layout from '@/components/layout';
+import CmdPalette from '@/components/cmd-palette';
+import CmdPaletteProvider from 'src/providers/cmd-palette-provider';
 
-import "../style.css";
+import '../style.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
       <NextSeo
-        title="Lazar Nikolov - Developer, designer, course creator."
-        description="Full-stack Engineer, UI Designer, and Open Source Advocate."
+        title='Lazar Nikolov - Developer, designer, course creator.'
+        description='Full-stack Engineer, UI Designer, and Open Source Advocate.'
         twitter={{
-          cardType: "summary_large_image",
-          handle: "@NikolovLazar",
+          cardType: 'summary_large_image',
+          handle: '@NikolovLazar',
         }}
         openGraph={{
-          url: "https://v1.nikolovlazar.com",
-          title: "Lazar Nikolov - Developer, designer, course creator.",
+          url: 'https://v1.nikolovlazar.com',
+          title: 'Lazar Nikolov - Developer, designer, course creator.',
           description:
-            "Full-stack Engineer, UI Designer, and Open Source Advocate.",
-          locale: "en_US",
+            'Full-stack Engineer, UI Designer, and Open Source Advocate.',
+          locale: 'en_US',
           images: [
             {
-              url: "https://v1.nikolovlazar.com/assets/images/social.png",
+              url: 'https://v1.nikolovlazar.com/assets/images/social.png',
               width: 1200,
               height: 630,
-              alt: "Lazar Nikolov",
-              type: "image/png",
+              alt: 'Lazar Nikolov',
+              type: 'image/png',
             },
           ],
         }}
       />
       <CmdPaletteProvider>
+        <chakra.a
+          href='https://nikolovlazar.com'
+          __css={{
+            height: '60px',
+            position: 'fixed',
+            bottom: 0,
+            insetX: 0,
+            zIndex: 'tooltip',
+            backgroundImage:
+              'repeating-linear-gradient(-55deg, #000, #000 20px, #ffb101 20px, #ffb101 40px)',
+            color: 'white',
+            fontWeight: 'bold',
+            fontSize: '2xl',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textShadow: '0 0 10px #000',
+          }}
+        >
+          {' '}
+          NOT MAINTAINED ANYMORE. Please visit my new website at:
+          nikolovlazar.com
+        </chakra.a>
         <Layout>
           <CmdPalette />
           <Component {...pageProps} />
